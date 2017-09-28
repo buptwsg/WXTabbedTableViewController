851e9e756d41031abae334a485df8fd445426535
sulirong <15528290768@163.com>
2017-09-28 11:12:10 +0800

    redo framework target

---
+++ CarthageSupport/CarthageSupport.h
@@ -0,0 +1,19 @@
+//
+//  CarthageSupport.h
+//  CarthageSupport
+//
+//  Created by sulirong on 2017/9/28.
+//  Copyright © 2017年 Shuguang Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageSupport.
+FOUNDATION_EXPORT double CarthageSupportVersionNumber;
+
+//! Project version string for CarthageSupport.
+FOUNDATION_EXPORT const unsigned char CarthageSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageSupport/PublicHeader.h>
+
+
